4ed1e6c8351b7e419a159be41cf6d14a0d29b5a9
Sourav sarkar <153831922+Sourav-6763@users.noreply.github.com>
2025-06-28 21:02:17 +0530

    Time: 41 ms (10.31%), Space: 46.5 MB (9.68%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String[] findRelativeRanks(int[] score) {   
+         List<List<Integer>> ab = new ArrayList<>();
+         int idx=0;
+        for(int i:score){
+            ab.add(Arrays.asList(i,idx++));
+        }
+        
+        ab.sort((a, b) -> b.get(0) - a.get(0));
+        String [] abc=new String[score.length];
+        int count=4;
+        for(int i=0;i<score.length;i++){
+            int ap=ab.get(i).get(1);
+            if(i==0){
+            abc[ap]="Gold Medal";
+            }
+            else if(i==1){
+            abc[ap]="Silver Medal";
+            }
+            else if(i==2){
+            abc[ap]="Bronze Medal";
+            }
+            else{
+                abc[ap]=String.valueOf(count);
+                count++;
+            }
+
+        }for(int i=0;i<abc.length;i++){
+            System.out.println(abc[i]);
+        }
+        return abc;
+
+    }
+}
