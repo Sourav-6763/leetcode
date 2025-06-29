ae40ac1d0b23584b7353e9c9c72f5751447f5fdf
Sourav sarkar <153831922+Sourav-6763@users.noreply.github.com>
2025-06-29 16:30:01 +0530

    Time: 3 ms (98.37%), Space: 44.3 MB (71.4%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,72 +1,19 @@
-class Solution {
-    public String intToRoman(int num) {
+public class Solution {
+    // Arrays for Roman numeral values and symbols
+    private static final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] numerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    public static String intToRoman(int num) {
         StringBuilder sb = new StringBuilder();
 
-        while (num >= 1000) {
-            sb.append("M");
-            num -= 1000;
-        }
-
-        if (num >= 900) {
-            sb.append("CM");
-            num -= 900;
-        }
-
-        if (num >= 500) {
-            sb.append("D");
-            num -= 500;
-        }
-
-        if (num >= 400) {
-            sb.append("CD");
-            num -= 400;
-        }
-
-        while (num >= 100) {
-            sb.append("C");
-            num -= 100;
-        }
-
-        if (num >= 90) {
-            sb.append("XC");
-            num -= 90;
-        }
-
-        if (num >= 50) {
-            sb.append("L");
-            num -= 50;
-        }
-
-        if (num >= 40) {
-            sb.append("XL");
-            num -= 40;
-        }
-
-        while (num >= 10) {
-            sb.append("X");
-            num -= 10;
-        }
-
-        if (num == 9) {
-            sb.append("IX");
-            num -= 9;
-        }
-
-        if (num >= 5) {
-            sb.append("V");
-            num -= 5;
-        }
-
-        if (num == 4) {
-            sb.append("IV");
-            num -= 4;
-        }
-
-        while (num >= 1) {
-            sb.append("I");
-            num -= 1;
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                sb.append(numerals[i]);
+            }
         }
 
         return sb.toString();
     }
-}
+
+}  
