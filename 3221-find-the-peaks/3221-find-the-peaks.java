47a3f08672a7315994ee8e6daf49a6b1d1f38625
Sourav sarkar <153831922+Sourav-6763@users.noreply.github.com>
2025-07-04 13:52:48 +0530

    Time: 1 ms (100%), Space: 44.8 MB (19.17%) - LeetHub

--- 3221-find-the-peaks/3221-find-the-peaks.java
+++ 3221-find-the-peaks/3221-find-the-peaks.java
@@ -1,15 +1,11 @@
 class Solution {
     public List<Integer> findPeaks(int[] mountain) {
-        ArrayList<Integer> peak=new ArrayList<>();
-       
-        
+        ArrayList<Integer> ab=new ArrayList<>();
         for(int i=1;i<=mountain.length-2;i++){
-            if(mountain[i-1]<mountain[i] && mountain[i+1]<mountain[i]){
-                peak.add(i);
-            
-
+            if(mountain[i-1]<mountain[i] && mountain[i]>mountain[i+1]){
+                ab.add(i);
             }
         }
-        return peak;
+        return ab;
     }
 }
