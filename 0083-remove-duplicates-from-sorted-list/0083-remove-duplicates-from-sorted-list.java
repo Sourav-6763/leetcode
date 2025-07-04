15c2ec6ff42bc6b9cafa6bf03dcb736f43fa7f82
Sourav sarkar <153831922+Sourav-6763@users.noreply.github.com>
2025-07-04 23:50:49 +0530

    Time: 0 ms (100%), Space: 44.5 MB (12.22%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -14,12 +14,13 @@ class Solution {
         if(head.next==null) return head;
         ListNode current=head;
         while(current != null && current.next !=null){
-            if(current.next.val==current.val){
+            if(current.val==current.next.val){
                 current.next=current.next.next;
             }
             else{
-            current=current.next;
+                current=current.next;
             }
+            
         }
         return head;
     }
