9bba11c64e7a260eea3b5e1a6994a33e42d9b771
Sourav sarkar <153831922+Sourav-6763@users.noreply.github.com>
2025-07-19 23:52:21 +0530

    Time: 9 ms (49.6%), Space: 44.8 MB (89.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,17 +1,30 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int idx=0;
-        int []result=new int[2];
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(target-nums[i])){
-                int res=target-nums[i];
-                result[0]=map.get(res);
-                result[1]=i;
-            }else{
-                map.put(nums[i],idx++);
+    public int[] twoSum(int[] nums, int sum) {
+        int n=nums.length;
+          int[][] arr = new int[n][2];
+
+        // Store value and original index
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
+        }
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+        int left=0;
+        int right=arr.length-1;
+        int [] res=new int[2];
+        while(left<right){
+            int val=arr[left][0]+arr[right][0];
+            if(sum==val){
+                res[0]=arr[left][1];
+                res[1]=arr[right][1];
+            }
+            if(sum<val){
+                right--;
+            }
+            else{
+                left++;
             }
         }
-        return result;
+        return res;
     }
 }
