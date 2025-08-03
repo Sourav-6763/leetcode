357772582fdc89ac0f37afea0b53fc702abe2c94
Sourav sarkar <153831922+Sourav-6763@users.noreply.github.com>
2025-08-03 13:20:55 +0530

    Time: 3 ms (98.27%), Space: 44.2 MB (83.47%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -3,14 +3,15 @@ public class Solution {
     private static final String[] numerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
     public static String intToRoman(int num) {
-        StringBuilder res = new StringBuilder();
-       for(int i=0;i<values.length;i++){
-        while(num>=values[i]){
-             num-=values[i];
-             res.append(numerals[i]);
+        StringBuilder res=new StringBuilder();
+         int i=0;
+        while(num>0){
+            while(num>=values[i]){
+                num=num-values[i];
+                res.append(numerals[i]);
+            }
+            i++;
         }
-       }
        return res.toString();
     }
-
 }  
