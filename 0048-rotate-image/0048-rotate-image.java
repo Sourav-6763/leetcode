f2b12c4e724d2a321c6cbbe249e05775301f5a9d
Sourav sarkar <153831922+Sourav-6763@users.noreply.github.com>
2025-08-05 23:19:36 +0530

    Time: 0 ms (100%), Space: 42.4 MB (40.98%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -3,19 +3,17 @@ class Solution {
         int rows = matrix.length;
         int column = matrix[0].length;
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = i + 1; j < rows; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+        for(int i=0;i<rows;i++){
+            for(int j=i+1;j<column;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
             }
         }
-
-      
-        
+    
         for(int i=0;i<rows;i++){
             int left=0;
-            int right=rows-1;
+            int right=column-1;
             while(left<right){
                 int temp=matrix[i][right];
                 matrix[i][right]=matrix[i][left];
@@ -24,7 +22,8 @@ class Solution {
                 right--;
             }
         }
-        
+      
+    
 
     }
 }
