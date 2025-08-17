4204667d59d870edc1bc79667cc041735309740b
Sourav sarkar <153831922+Sourav-6763@users.noreply.github.com>
2025-08-17 18:08:34 +0530

    Time: 11 ms (1.32%), Space: 42.4 MB (9.39%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,44 +1,18 @@
+import java.math.BigInteger;
 class Solution {
     public int[] plusOne(int[] digits) {
-        int index = digits.length - 1;
-        int count = 0;
-        for (int i = 0; i < digits.length; i++) {
-            if (digits[i] == 9) {
-                count++;
-            }
+        String num="";
+        for(int i:digits){
+            num+=i;
         }
-
-        if (digits[index] == 9 && count != digits.length) {
-            for (int i = index; i >= 0; i--) {
-                if (digits[i] != 9) {
-                    int dig = digits[i];
-                    int p = dig + 1;
-                    digits[i] = p;
-                    return digits;
-                } else if (digits[i] == 9) {
-                    digits[i] = 0;
-                }
-
-            }
+        BigInteger numA = new BigInteger(num);
+           numA = numA.add(BigInteger.ONE);
+        String res=numA.toString();
+       int[] result = new int[res.length()];
+        for (int i = 0; i < res.length(); i++) {
+            result[i] = Character.getNumericValue(res.charAt(i));
         }
-        if (digits[index] == 9 && count == digits.length) {
-            int[] digitnew = new int[digits.length + 1];
-            for (int i = digitnew.length - 1; i >= 0; i--) {
-                if (i == 0) {
-                    digitnew[i] = 1;
-                } else {
-                    digitnew[i] = 0;
-                }
-
-            }
-            return digitnew;
-
-        } else {
-            int a = digits[index];
-            int res = a + 1;
-            digits[index] = res;
-            return digits;
-        }
-      
+   
+        return result;
     }
 }
