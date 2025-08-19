283dd74044440ddb0565e930fe9cb76af950ba14
Sourav sarkar <153831922+Sourav-6763@users.noreply.github.com>
2025-08-19 18:06:47 +0530

    Time: 2 ms (83.18%), Space: 44.3 MB (97.28%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,20 +1,21 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res =new ArrayList<>();
-        solver(candidates,0,res,new ArrayList<>(),target);
+        List<List<Integer>> res=new ArrayList<>();
+        solver(candidates,target,res,new ArrayList<>(),0);
         return res;
     }
-    public void solver(int[]candidates,int idx,List<List<Integer>> res,List<Integer>path,int target){
-         if (idx == candidates.length || target < 0) {
-            return;
-        }
-        if(target==0){
-            res.add(new ArrayList<>(path));
-            return;
-        }
-        path.add(candidates[idx]);
-        solver(candidates,idx,res,path,target-candidates[idx]);
-        path.remove(path.size()-1);
-        solver(candidates, idx + 1, res, path, target);
+
+    public void solver(int [] candidates, int target, List<List<Integer>> res,ArrayList<Integer> demo,int idx){
+if(idx==candidates.length || target<0){
+return ;
+}
+if(target==0){
+   res.add(new ArrayList<>(demo));
+   return;
+}
+demo.add(candidates[idx]);
+solver(candidates,target-candidates[idx],res,demo,idx);
+demo.remove(demo.size()-1);
+solver(candidates,target,res,demo,idx+1);
     }
 }
