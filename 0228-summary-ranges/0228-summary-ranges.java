37df56c89f0a250870cc758f9ff7234ac55b55b4
Sourav sarkar <153831922+Sourav-6763@users.noreply.github.com>
2025-09-08 20:38:57 +0530

    Time: 5 ms (83.84%), Space: 42.1 MB (13.38%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -3,14 +3,13 @@ class Solution {
         List<String> res=new ArrayList<>();
         for(int i=0;i<nums.length;i++){
             int start=nums[i];
-            while(nums.length>i+1 &&nums[i+1]-nums[i]==1){
+            while(i<nums.length-1 && nums[i+1]-nums[i]==1){
                 i++;
             }
-            if(start!=nums[i]){
-                res.add(start +"->"+nums[i]);
-            }
-            else{
+            if(start==nums[i]){
                 res.add(Integer.toString(start));
+            }else{
+                res.add(start+"->"+nums[i]);
             }
         }
         return res;
