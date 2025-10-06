2c133fd0d4a3a10720bdcaef03aa3b67d5049d26
Sourav sarkar <153831922+Sourav-6763@users.noreply.github.com>
2025-10-06 15:38:53 +0530

    Time: 4 ms (5.82%), Space: 60 MB (5.84%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,18 +1,24 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         ArrayList<Integer> pree = new ArrayList<>();
-        ArrayList<Integer> suff = new ArrayList<>(Collections.nCopies(nums.length, 1));
+        int [] res=new int[nums.length];
+        int [] suff=new int[nums.length];
+        for(int i=0;i<suff.length;i++){
+            suff[i]=1;
+        }
         pree.add(1);
-        for (int i = 1; i < nums.length; i++) {
-            pree.add(pree.get(i - 1) * nums[i - 1]);
+        for(int i=1;i<nums.length;i++){
+            pree.add(nums[i-1]*pree.get(i-1));
         }
-        for (int i = nums.length - 2; i >= 0; i--) {
-            suff.set(i, suff.get(i + 1) * nums[i + 1]);
+        for(int i=nums.length-2;i>=0;i--){
+            suff[i]=nums[i+1]*suff[i+1];
         }
-        int[] result = new int[nums.length];
-      for(int i=0;i<nums.length;i++){
-         result[i]=pree.get(i)*suff.get(i);
-      }
-        return result;
+        for(int i=0;i<res.length;i++){
+          res[i]=pree.get(i)*suff[i];
+        }
+        
+        
+        return res;
+       
     }
 }
