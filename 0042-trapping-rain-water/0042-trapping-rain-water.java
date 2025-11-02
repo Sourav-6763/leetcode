3ef1c686936ba7fcf005791bd53f1329e23a52aa
Sourav sarkar <153831922+Sourav-6763@users.noreply.github.com>
2025-11-02 19:01:55 +0530

    Time: 1 ms (58.37%), Space: 48.8 MB (5.47%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,23 +1,23 @@
 class Solution {
-    public int trap(int[] height) {
-        int n = height.length - 1;
+    public int trap(int[] arr) {
         int sum=0;
-        List<Integer> left = new ArrayList<>();
-        List<Integer> right = new ArrayList<>();
-        int maxL = height[0];
-        for (int i = 0; i < height.length; i++){
-            maxL=Math.max(height[i],maxL);
-            left.add(maxL);
+        int n=arr.length;
+        int[] leftMax =new int [n];
+        int[] rightMax =new int [n];
+        int lmax=arr[0];
+        for(int i=0;i<n;i++){
+            lmax=Math.max(lmax,arr[i]);
+            leftMax[i]=lmax;
         }
-        int maxR = height[n];
-        for (int i = n; i >=0; i--){
-            maxR=Math.max(height[i],maxR);
-            right.add(maxR);
+        int rmax=arr[n-1];
+        for(int i=n-1;i>=0;i--){
+            rmax=Math.max(rmax,arr[i]);
+            rightMax[i]=rmax;
         }
-        Collections.reverse(right);
-        for(int i=0;i<height.length;i++){
-            int min=Math.min(left.get(i),right.get(i));
-            sum+=min-height[i];
+        for(int i=0;i<n;i++){
+            int min=Math.min(leftMax[i],rightMax[i]);
+            sum+=min-arr[i];
+            // System.out.println(rightMax[i]);
         }
         return sum;
     }
