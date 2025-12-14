ec7f8ce743c4f0cb78ecd829ee87cdfc0f6427c4
Sourav sarkar <153831922+Sourav-6763@users.noreply.github.com>
2025-12-14 23:48:12 +0530

    Time: 8 ms (7.34%), Space: 44 MB (43.61%) - LeetHub

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int missingInteger(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            // if(map.containKey(nums[i])){
+
+            // }else{
+                map.put(nums[i],1);
+            // }
+        }
+       int sum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]-1==nums[i-1]){
+                sum+=nums[i];
+            }else{
+                break;
+            }
+        }
+        System.out.println(sum);
+        while(map.containsKey(sum)){
+          sum++; 
+        }
+        return sum;
+    }
+}
