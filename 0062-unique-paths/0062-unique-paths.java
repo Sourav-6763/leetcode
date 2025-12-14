b0ea31b60f5dae093c87f19c1306674a87a6f7d9
Sourav sarkar <153831922+Sourav-6763@users.noreply.github.com>
2025-12-14 15:57:19 +0530

    Time: 0 ms (100%), Space: 42 MB (43.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,33 +1,16 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] t = new int[m][n];
-        
-        // Initialize all cells to -1
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                t[i][j] = -1;
+        int gird[][]=new int [m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0){
+                    gird[i][j]=1;
+                }
+                else{
+                    gird[i][j]=gird[i-1][j]+gird[i][j-1];
+                }
             }
         }
-        
-        return solver(0, 0, m, n, t);
-    }
-
-    public int solver(int i, int j, int m, int n, int[][] t) {
-        // Base case: reached destination
-        if (i == m - 1 && j == n - 1) return 1;
-
-        // Out of bounds
-        if (i >= m || j >= n) return 0;
-
-        // If already computed
-        if (t[i][j] != -1) return t[i][j];
-
-        // Recursive calls: move right and down
-        int right = solver(i, j + 1, m, n, t);
-        int down = solver(i + 1, j, m, n, t);
-
-        // Store result in memoization table
-        t[i][j] = right + down;
-        return t[i][j];
+        return gird[m-1][n-1];
     }
 }
